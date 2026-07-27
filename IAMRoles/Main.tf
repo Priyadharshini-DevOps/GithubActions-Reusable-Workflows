816fba70951496a31de5a05e763c85f928c51202
Priya Nobel <priyadharshini04arumugam@gmail.com>
2026-07-27 09:03:31 +0530

    changes done

--- IAMRoles/Main.tf
+++ IAMRoles/Main.tf
@@ -7,10 +7,10 @@ terraform {
 
 	backend "remote" {
 		hostname = "app.terraform.io"
-		organization = "CloudQuickLabs"
+		organization = "Nobelyuga"
 
 		workspaces {
-			name = "AWSBackup"
+			name = "Secrets"
 		}
 	}
 }
